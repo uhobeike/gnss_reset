dac12f8329e6ffeea679f8afc15dfadddbbe14cc
Tatsuhiro Ikebe <beike315@icloud.com>
2023-05-10 10:27:12 +0900

    Feat map cb (#4)
    * Update rviz config
    
    * Add embed_gnss2map.launch.py
    
    * Feature mapCb

--- src/embed_gnss2map.cpp
+++ src/embed_gnss2map.cpp
@@ -2,46 +2,43 @@
 
 namespace embed_gnss2map
 {
-EmbedGnss2MapNode::EmbedGnss2MapNode() : Node("embed_gnss2map_node")
-{
-  initPublisher();
-  publishMapWithGnss();
-}
+EmbedGnss2MapNode::EmbedGnss2MapNode() : Node("embed_gnss2map_node") { initPubSub(); }
 
-void EmbedGnss2MapNode::initPublisher()
+void EmbedGnss2MapNode::initPubSub()
 {
   pub_map_with_gnss_ = create_publisher<nav_msgs::msg::OccupancyGrid>("map_with_gnss", 1);
+
+  auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+  qos_profile.reliability(rclcpp::ReliabilityPolicy::Reliable);
+  qos_profile.durability(rclcpp::DurabilityPolicy::TransientLocal);
+  sub_map_ = create_subscription<nav_msgs::msg::OccupancyGrid>(
+      "map", qos_profile, std::bind(&EmbedGnss2MapNode::mapCb, this, std::placeholders::_1));
 }
 
 void EmbedGnss2MapNode::embedGnss2Map() {}
+
+void EmbedGnss2MapNode::mapCb(nav_msgs::msg::OccupancyGrid::ConstSharedPtr msg)
+{
+  map_with_gnss_.header = msg->header;
+  map_with_gnss_.info = msg->info;
+  int map_size = msg->info.width * msg->info.height;
+  map_with_gnss_.data.resize(map_size);
+  std::copy(std::begin(msg->data), std::end(msg->data), std::begin(map_with_gnss_.data));
+
+  publishMapWithGnss();
+}
 
 void EmbedGnss2MapNode::publishMapWithGnss()
 {
   auto occupancy_grid = nav_msgs::msg::OccupancyGrid();
 
   occupancy_grid.header.stamp = now();
-  occupancy_grid.header.frame_id = "map";
-
+  occupancy_grid.header = map_with_gnss_.header;
   occupancy_grid.info.map_load_time = now();
-  occupancy_grid.info.resolution = 0.05;
-  occupancy_grid.info.width = 100;
-  occupancy_grid.info.height = 100;
+  occupancy_grid.info = map_with_gnss_.info;
 
-  occupancy_grid.info.origin.position.x = 0.0;
-  occupancy_grid.info.origin.position.y = 0.0;
-  occupancy_grid.info.origin.position.z = 0.0;
-  occupancy_grid.info.origin.orientation.x = 0.0;
-  occupancy_grid.info.origin.orientation.y = 0.0;
-  occupancy_grid.info.origin.orientation.z = 0.0;
-  occupancy_grid.info.origin.orientation.w = 1.0;
-
-  int map_size = occupancy_grid.info.width * occupancy_grid.info.height;
+  int map_size = map_with_gnss_.info.width * map_with_gnss_.info.height;
   occupancy_grid.data.resize(map_size);
-  map_with_gnss_.data.resize(map_size);
-
-  for (uint32_t row = 0; row < occupancy_grid.info.height; ++row)
-    for (uint32_t col = 0; col < occupancy_grid.info.width; ++col)
-      map_with_gnss_.data[row * occupancy_grid.info.width + col] = 0;
 
   std::copy(std::begin(map_with_gnss_.data), std::end(map_with_gnss_.data),
             std::begin(occupancy_grid.data));
