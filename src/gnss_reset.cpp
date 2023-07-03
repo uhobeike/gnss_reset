e82bda79eddef8938d8b67a54559f71d35fd8cb4
uhobeike <beike315@icloud.com>
2023-07-03 21:33:55 +0900

    Feature odom2map

--- src/gnss_reset.cpp
+++ src/gnss_reset.cpp
@@ -4,9 +4,20 @@
 #include "gnss_reset/gnss_reset.hpp"
 
 #include <Eigen/Dense>
+#include <geometry_msgs/msg/transform_stamped.hpp>
 
 #include "rosbag2_cpp/converter_interfaces/serialization_format_converter.hpp"
 #include "rosbag2_cpp/typesupport_helpers.hpp"
+#include "tf2/LinearMath/Transform.h"
+#include "tf2/convert.h"
+#include "tf2/time.h"
+#include "tf2/utils.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/create_timer_ros.h"
+#include "tf2_ros/message_filter.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_ros/transform_listener.h"
 
 using namespace std::chrono_literals;
 
@@ -36,6 +47,22 @@ void GnssResetNode::initTimer()
   timer_ = create_wall_timer(1s, std::bind(&GnssResetNode::publishMapWithGnss, this));
 }
 
+void GnssResetNode::initTf()
+{
+  tf_broadcaster_.reset();
+  tf_listener_.reset();
+  tf_buffer_.reset();
+
+  tf_buffer_ = std::make_shared<tf2_ros::Buffer>(get_clock());
+  auto timer_interface = std::make_shared<tf2_ros::CreateTimerROS>(
+    get_node_base_interface(), get_node_timers_interface(),
+    create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive, false));
+  tf_buffer_->setCreateTimerInterface(timer_interface);
+  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+  tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(shared_from_this());
+  latest_tf_ = tf2::Transform::getIdentity();
+}
+
 void GnssResetNode::setParam() { declare_parameter("read_rosbag_path", "map_with_gnss"); }
 
 void GnssResetNode::getParam()
@@ -45,10 +72,12 @@ void GnssResetNode::getParam()
 
 void GnssResetNode::gnssCb(sensor_msgs::msg::NavSatFix::ConstSharedPtr msg)
 {
+  gnss_stamp_.stamp = msg->header.stamp;
   if (get_map_) {
     static bool once_flag = true;
     if (once_flag) {
       publishMapWithGnss();
+      initTf();
       once_flag = false;
     }
     gnssReset(*msg);
@@ -59,6 +88,7 @@ void GnssResetNode::gnssReset(sensor_msgs::msg::NavSatFix msg)
 {
   auto index = findNearestLatLong(msg);
   debugIndexToMap(index);
+  odom2map();
 }
 
 void GnssResetNode::debugIndexToMap(int index)
@@ -69,6 +99,38 @@ void GnssResetNode::debugIndexToMap(int index)
   map.data[index] = 99;
 
   pub_debug_map_->publish(map);
+}
+
+void GnssResetNode::odom2map()
+{
+  geometry_msgs::msg::PoseStamped odom_to_map;
+  try {
+    tf2::Quaternion q;
+    q.setRPY(0, 0, 0);
+    tf2::Transform tmp_tf(q, tf2::Vector3(9.45171, 11.5139, 0.0));
+
+    geometry_msgs::msg::PoseStamped tmp_tf_stamped;
+    tmp_tf_stamped.header.frame_id = "base_footprint";
+    tmp_tf_stamped.header.stamp = gnss_stamp_.stamp;
+
+    tf2::toMsg(tmp_tf.inverse(), tmp_tf_stamped.pose);
+
+    tf_buffer_->transform(tmp_tf_stamped, odom_to_map, "odom");
+  } catch (tf2::TransformException & e) {
+    RCLCPP_DEBUG(get_logger(), "%s", e.what());
+    return;
+  }
+
+  auto stamp = tf2_ros::fromMsg(gnss_stamp_.stamp);
+  tf2::TimePoint transform_tolerance_ = stamp + tf2::durationFromSec(1.0);
+
+  tf2::impl::Converter<true, false>::convert(odom_to_map.pose, latest_tf_);
+  geometry_msgs::msg::TransformStamped tmp_tf_stamped;
+  tmp_tf_stamped.header.frame_id = "map";
+  tmp_tf_stamped.header.stamp = tf2_ros::toMsg(transform_tolerance_);
+  tmp_tf_stamped.child_frame_id = "odom";
+  tf2::impl::Converter<false, true>::convert(latest_tf_.inverse(), tmp_tf_stamped.transform);
+  tf_broadcaster_->sendTransform(tmp_tf_stamped);
 }
 
 unsigned int GnssResetNode::findNearestLatLong(sensor_msgs::msg::NavSatFix target_gnss_data)
